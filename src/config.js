19cf97d6ba21011e636898d9d710944ac75fc957
shwethats <shwethats095@gmail.com>
2021-07-30 22:07:08 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const IS_DEBUG = process.env.NODE_ENV === 'development';
+
+export const REACT_APP_ROUTER_BASE = process.env.REACT_APP_ROUTER_BASE || '/appies_assignment/';
+
+export const IS_TEST = process.env.NODE_ENV === 'test';
+
